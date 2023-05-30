bf2db6fd5ed3124e6ab57c6e78d65a8a9531405a
shripad-jyothinath <shripadsuper@gmail.com>
2023-05-30 12:15:47 +0530

    Cherish OS bringup

--- cherish_hotdog.mk
+++ cherish_hotdog.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/hotdog/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_hotdog
+PRODUCT_NAME := cherish_hotdog
 PRODUCT_DEVICE := hotdog
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := HD1917
@@ -25,11 +25,24 @@ CUSTOM_DEVICE := OnePlus7TPro
 PRODUCT_SYSTEM_NAME := OnePlus7TPro
 PRODUCT_SYSTEM_DEVICE := OnePlus7TPro
 
-# Pixel Stuff
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+# Inherit some common cherish stuff.
+TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+IS_PHONE := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_ENABLE_BLUR := false
 
+# Maintainer Flag
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Shripad Gamer
+
+# OFFICIAL
+CHERISH_BUILD_TYPE := UNOFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
